fc667002d37b0affbe0cc4f7325ebe43674b9d7c
Leandro Diomar Gomes da Silva <leandrodiomar123@gmail.com>
2020-08-03 20:46:43 -0300

    Capacidade da tabela de demonstrar dados

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,9 +15,12 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
 
+	
+	
 	@FXML
 	private MenuItem menuItemSeller;
 	
@@ -27,6 +30,8 @@ public class MainViewController implements Initializable {
 	@FXML
 	private MenuItem menuItemAbout;
 	
+	
+	
 	@FXML
 	public void onMenuItemSellerAcntion() {
 		System.out.println("Menu item seller pressed");
@@ -34,14 +39,13 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDepartmentAcntion() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
 	public void onMenuItemAboutAcntion() {
 		loadView("/gui/About.fxml");
 	}
-	
 	
 	
 	@Override
@@ -61,6 +65,30 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 			
+		} catch (IOException e) {
+			
+			Alerts.showAlert("IO Exception ", "Erro ao iniciar nova view",e.getMessage() ,AlertType.ERROR );
+		}
+		
+	}
+	
+	private synchronized void loadView2(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			DepartmentListController controller = loader.getController();
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+					
 		} catch (IOException e) {
 			
 			Alerts.showAlert("IO Exception ", "Erro ao iniciar nova view",e.getMessage() ,AlertType.ERROR );
