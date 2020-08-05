69c0ff4fa69dedc3a61f97526ecd9f3038a3dd72
Leandro Diomar Gomes da Silva <leandrodiomar123@gmail.com>
2020-08-05 18:54:11 -0300

    Criançao de 'Seller' usando 'Department de base

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
+import model.services.SellerService;
 
 public class MainViewController implements Initializable {
 
@@ -35,7 +36,10 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemSellerAcntion() {
-		System.out.println("Menu item seller pressed");
+		loadView("/gui/SellerList.fxml", (SellerListController controller)->{
+			controller.setSellerService(new SellerService());
+			controller.updateTableView();
+		});
 	}
 	
 	@FXML
